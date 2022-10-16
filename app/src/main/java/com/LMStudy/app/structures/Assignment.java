9844c9edbe82aea599ec35596afd80f8e4539ad8
yulying <yy541@scarletmail.rutgers.edu>
2022-10-16 15:46:28 -0400

    Created base functions for assignment and work queue. Created a student class that stores student information (canvas student ID)

--- app/src/main/java/com/LMStudy/app/structures/Assignment.java
+++ app/src/main/java/com/LMStudy/app/structures/Assignment.java
@@ -1,8 +1,105 @@
 package com.LMStudy.app.structures;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
- * Data unit representing a work task.
+ * Data unit representing a general task.
+ * Will separate different assignment tasks into individual classes (e.g. Exams, Study Sessions, etc.)
  */
-public class Assignment {
+public class Assignment implements Comparable<Assignment> {
+
+    private static final int INITIAL_PRIORITY = 2;
+    private static final int INITIAL_TIMEBLOCK = 1; //hours set for assignment per session
+
+    private String courseInfo; // go to canvas to get course number
+    private String assignmentName;
+    private int priority; // priority is between 1-10
+    private String assignmentType; // create an assignment enum
+    //private Date dueDate; // convert to Date class
+    private LocalDateTime dueDate;
+    private int assignedTimeBlocks; // for student
+
+    /**
+     * Creates an Assignment object that stores assignment information.
+     * @param courseInfo Course number/Class name
+     * @param assignmentName Name of assignment
+     * @param assignmentType Assignment type (normal, exam, project, etc.)
+     * @param dueDate Due date of assignment
+     */
+    public Assignment(String courseInfo, String assignmentName, String assignmentType, LocalDateTime dueDate) {
+        this.courseInfo = courseInfo;
+        this.assignmentName = assignmentName;
+        priority = INITIAL_PRIORITY;
+        this.assignmentType = assignmentType;
+        this.dueDate = dueDate;
+        assignedTimeBlocks = INITIAL_TIMEBLOCK;
+    }
+
+    // May delete any unnecessary get functions
+    public String getCourseInfo() {
+        return courseInfo;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getAssignmentType() {
+        return assignmentType;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    /**
+     * Changes the priority of the assignment. Might be available to students only for
+     * self-assigned priority or based on schedule.
+     * @param priority Assignment priority.
+     */
+    public void changePriority(int priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * Changes the due date of the assignment. Available only to teachers.
+     * @param dueDate Due date of assignment.
+     */
+    public void changeDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isEqual(Assignment assignment) {
+        if (courseInfo.compareTo(assignment.courseInfo) == 0 && assignmentName.compareTo(assignment.assignmentName) == 0
+                && assignmentType.compareTo(assignment.assignmentType) == 0 && dueDate.compareTo(assignment.dueDate) == 0) {
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "DUE " + dueDate + ": (" + courseInfo + ") " + assignmentName;
+    }
+
+    @Override
+    public int compareTo(Assignment assignment) {
+        if (dueDate.compareTo(assignment.dueDate) == 0) {
+            return Integer.compare(priority, assignment.priority)*(-1);
+        }
+        return dueDate.compareTo(assignment.dueDate);
+    }
+
+    /**
+     * Just a testing main function.
+     * @param args
+     */
+    public static void main (String[] args) {
+        System.out.println("DUE DATE:\n(COURSE NAME)");
+    }
 }
--- app/src/main/java/com/LMStudy/app/structures/WorkQueue.java
+++ app/src/main/java/com/LMStudy/app/structures/WorkQueue.java
@@ -1,16 +1,74 @@
 package com.LMStudy.app.structures;
 
+import java.time.LocalDateTime;
 import java.util.TreeSet;
 
 /**
- * Queue for managing Student User assignments.
+ * Queue for managing general assignments
  */
-public class WorkQueue {
+public class WorkQueue{
    private TreeSet<Assignment> items;
+   private long userId;
 
-   private WorkQueue instance = new WorkQueue();
+   //private WorkQueue instance = new WorkQueue();
 
-   private WorkQueue() {
+   /**
+    * User's work queue
+    * @param userId Assigned ID of user as received from Canvas.
+    */
+   public WorkQueue(long userId) {
+      items = new TreeSet<>();
+      this.userId = userId;
+   }
 
+   public TreeSet<Assignment> getWorkQueue() {
+      return items;
+   }
+
+   public boolean addToQueue(Assignment assignment) {
+      if (!items.contains(assignment)) {
+         return items.add(assignment);
+      }
+      return false;
+   }
+
+   public boolean removeFromQueue(Assignment assignment) {
+      if (items.contains(assignment)) {
+         return items.remove(assignment);
+      }
+      return false;
+   }
+
+   public Assignment getFirstAssignment() {
+      if (items.isEmpty() != true) {
+         return items.first();
+      }
+
+      return null; // Create a new study session assignment?
+   }
+
+   public void getAllAssignments() {
+      for (Assignment a : items) {
+         System.out.println(a);
+      }
+   }
+
+   /**
+    * Testing basic work queue functions
+    * @param args Input arguments
+    */
+   public static void main(String[] args) {
+      WorkQueue itemsList = new WorkQueue(1);
+      LocalDateTime firstDate = LocalDateTime.of(2022, 11, 03, 11, 59);
+      LocalDateTime secondDate = LocalDateTime.of(2022, 10, 31, 11, 59);
+      Assignment assignment1 = new Assignment("English", "First_Essay", "Essay", firstDate);
+      Assignment assignment2 = new Assignment("Math", "Practice_Set1", "HW", secondDate);
+      Assignment assignment3 = new Assignment("CS", "Project 1", "Project", secondDate);
+      assignment3.changePriority(6);
+      itemsList.addToQueue(assignment1);
+      itemsList.addToQueue(assignment2);
+      itemsList.addToQueue(assignment3);
+
+      itemsList.getAllAssignments();
    }
 }
