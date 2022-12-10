db59c34329548dd9c9ef3e5e74315712095b94f3
yulying <yy541@scarletmail.rutgers.edu>
2022-12-10 15:09:19 -0500

    Cleaned up classes a bit and added hardcoded strings to strings.xml file.

--- app/src/main/java/com/LMStudy/app/student/StudentHome.java
+++ app/src/main/java/com/LMStudy/app/student/StudentHome.java
@@ -45,28 +45,22 @@ import java.util.List;
  */
 public class StudentHome extends AppCompatActivity {
 
-   // TODO: WorkFlow item that stores all work item information
    private final WorkFlow flowLink = WorkFlow.getInstance();
    private final SyncService caller = SyncService.getInstance();
 
-   private List<WorkItem> itemsList; // Used for populating recycler view
+   private List<WorkItem> itemsList;
 
    private TextView currentAssignment;
 
    private AccountActivity.RecyclerAdapter studentHomeAdapter;
    private RecyclerView rcSchedule;
-   private ImageView profilePicture;
    private Button refreshButton, addAssignmentBtn, confirmAssignmentBtn;
 
-   private TextView assignmentNameText, assignmentNameInfo, assignmentAssigneeText, assignmentAssigneeInfo,
-           assignmentTypeText, assignmentTypeInfo, assignmentCourseText, assignmentCourseInfo,
-           assignmentDueDateText, assignmentDueDateInfo, assignmentPrioText, assignmentPrioInfo,
-           assignmentHoursText, assignmentHoursInfo;
+   private TextView assignmentNameInfo, assignmentTypeInfo, assignmentCourseInfo, assignmentDueDateInfo,
+           assignmentPrioInfo;
 
    private TextView newAssignmentName, newAssignmentHour, dateView;
    private Spinner newAssignmentTypeSpinner, newAssignmentCourseSpinner, newAssignmentPrioritySpinner;
-
-   // CALENDAR STUFF
 
    private DatePicker datePicker;
    private Calendar calendar;
@@ -78,9 +72,7 @@ public class StudentHome extends AppCompatActivity {
          public void onDateSet(DatePicker arg0,
                            int arg1, int arg2, int arg3) {
          // TODO Auto-generated method stub
-         // arg1 = year
-         // arg2 = month
-         // arg3 = day
+
          showDate(arg1, arg2+1, arg3);
 
          }
@@ -88,7 +80,6 @@ public class StudentHome extends AppCompatActivity {
 
    private Button completeAssignmentBtn, removeAssignmentBtn;
 
-   private TextView confirmRemovalText;
    private Button yesButton, noButton;
 
    ArrayList<NewCourse> enrolledCourseList = (ArrayList<NewCourse>) flowLink.getCourseList();
@@ -96,24 +87,17 @@ public class StudentHome extends AppCompatActivity {
 
    /**
     * Creates student home screen from action
-    * @param savedInstanceState
+    * @param savedInstanceState Saved application state
     */
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_student_home);
 
-      profilePicture = findViewById(R.id.profile_icon);
       rcSchedule = findViewById(R.id.assignment_work_queue);
       refreshButton = findViewById(R.id.refresh_Btn);
       addAssignmentBtn = findViewById(R.id.addAssignment_Btn);
       currentAssignment = findViewById(R.id.current_assignment_text);
-
-      /**** ARBITRARY ENROLLED TEST COURSE FOR STUDENT ****/
-      NewCourse testCourse = new NewCourse("999999","Personal Time","pw");
-
-      // enrolledCourseList gets enrolled course from WorkFlow above but courses in WorkFlow is currently null
-      enrolledCourseList.add(testCourse);
 
       setDisplay();
       rcSchedule.setLayoutManager(new LinearLayoutManager(this));
@@ -126,48 +110,34 @@ public class StudentHome extends AppCompatActivity {
 
          /**
           * Expands work item from recycler view to popup with more details about work item
-          * @param view
-          * @param position
+          * @param view The view within the AdapterView that was clicked
+          * @param position Position of the item within the RecyclerView list
           */
          @Override
          public void onItemClick(View view, int position) {
             WorkItem clickedItem = itemsList.get(position);
 
-            // inflate the layout of the popup window
             LayoutInflater inflater = (LayoutInflater)
                     getSystemService(LAYOUT_INFLATER_SERVICE);
             View popupView = inflater.inflate(R.layout.assignment_info_popup, null);
 
-            // create the popup window
             int width = LinearLayout.LayoutParams.WRAP_CONTENT;
             int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-            boolean focusable = true; // lets taps outside the popup also dismiss it
+            boolean focusable = true;
             final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
-            // show the popup window
-            // which view you pass in doesn't matter, it is only used for the window token
             popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
-            // dismiss the popup window when touched
             popupView.setOnTouchListener((v, event) -> {
                popupWindow.dismiss();
                return true;
             });
 
-            assignmentNameText = popupView.findViewById(R.id.assignmentName);
             assignmentNameInfo = popupView.findViewById(R.id.assignmentName_txt);
-            assignmentAssigneeText = popupView.findViewById(R.id.assigneeName);
-            assignmentAssigneeInfo = popupView.findViewById(R.id.assigneeName_txt);
-            assignmentTypeText = popupView.findViewById(R.id.assignmentType);
             assignmentTypeInfo = popupView.findViewById(R.id.assignmentType_txt);
-            assignmentCourseText = popupView.findViewById(R.id.course_txt);
             assignmentCourseInfo = popupView.findViewById(R.id.courseName_txt);
-            assignmentDueDateText = popupView.findViewById(R.id.dueDateInfo_title);
             assignmentDueDateInfo = popupView.findViewById(R.id.dueDateInfo_txt);
-            assignmentPrioText = popupView.findViewById(R.id.priorityInfo);
             assignmentPrioInfo = popupView.findViewById(R.id.priorityInfo_txt);
-            assignmentHoursText = popupView.findViewById(R.id.hoursInfo);
-            assignmentHoursInfo = popupView.findViewById(R.id.hoursInfo_txt);
 
             completeAssignmentBtn = popupView.findViewById(R.id.completeAssignment_btn);
             removeAssignmentBtn = popupView.findViewById(R.id.rmvAssignment_btn);
@@ -188,23 +158,18 @@ public class StudentHome extends AppCompatActivity {
                   getSystemService(LAYOUT_INFLATER_SERVICE);
                View popupView1 = inflater1.inflate(R.layout.remove_assignment_popup, null);
 
-               // create the popup window
                int width1 = LinearLayout.LayoutParams.WRAP_CONTENT;
                int height1 = LinearLayout.LayoutParams.WRAP_CONTENT;
-               boolean focusable1 = false; // lets taps outside the popup also dismiss it
+               boolean focusable1 = false;
                final PopupWindow popupWindow1 = new PopupWindow(popupView1, width1, height1, focusable1);
 
-               // show the popup window
-               // which view you pass in doesn't matter, it is only used for the window token
                popupWindow1.showAtLocation(view, Gravity.CENTER, 0, 0);
 
-               // dismiss the popup window when touched
                popupView1.setOnTouchListener((v, event) -> {
                   popupWindow1.dismiss();
                   return true;
                });
 
-               confirmRemovalText = popupView1.findViewById(R.id.confirmRemove_txt);
                yesButton = popupView1.findViewById(R.id.yes_btn);
                noButton = popupView1.findViewById(R.id.no_btn);
 
@@ -223,23 +188,16 @@ public class StudentHome extends AppCompatActivity {
                   popupWindow1.dismiss();
                });
             });
-
-            /**
-             * Same logic as complete assignment
-             */
-            removeAssignmentBtn.setOnClickListener(view1 -> {
-
-            });
          }
 
          @Override
          public void onItemLongClick(View view, int position) {
-            // do whatever
+
          }
       }));
 
       /**
-       * Refreshes the student home activity with refresh button.
+       * Refreshes the student home activity.
        */
       refreshButton.setOnClickListener(view -> {
          finish();
@@ -247,31 +205,26 @@ public class StudentHome extends AppCompatActivity {
       });
 
       /**
-       * Opens an add assignment popup with textviews, spinners and calendars for options.
+       * Opens an add assignment popup with textviews, spinners and calendars for options to
+       * prevent input errors.
        */
-      addAssignmentBtn.setOnClickListener(view -> { // Adding an assignment only shows in the specific student's view
-         // inflate the layout of the popup window
+      addAssignmentBtn.setOnClickListener(view -> {
          LayoutInflater inflater = (LayoutInflater)
                  getSystemService(LAYOUT_INFLATER_SERVICE);
          View popupView = inflater.inflate(R.layout.add_assignment_popup_revised, null);
 
-         // create the popup window
          int width = LinearLayout.LayoutParams.WRAP_CONTENT;
          int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-         boolean focusable = true; // lets taps outside the popup also dismiss it
+         boolean focusable = true;
          final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
-         // show the popup window
-         // which view you pass in doesn't matter, it is only used for the window token
          popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
-         // dismiss the popup window when touched
          popupView.setOnTouchListener((v, event) -> {
             popupWindow.dismiss();
             return true;
          });
 
-         // SPINNER THINGS
          newAssignmentName = popupView.findViewById(R.id.r_newAssignmentName_input);
          newAssignmentTypeSpinner = popupView.findViewById(R.id.assignment_type_spinner);
          newAssignmentCourseSpinner = popupView.findViewById(R.id.course_spinner);
@@ -288,7 +241,6 @@ public class StudentHome extends AppCompatActivity {
 
          confirmAssignmentBtn = popupView.findViewById(R.id.r_confirm_assignment_Btn);
 
-         // Used to populate Course spinner list
          for (NewCourse c : enrolledCourseList) {
             enrolledCourseNameList.add(c.toString());
          }
@@ -390,8 +342,8 @@ public class StudentHome extends AppCompatActivity {
    }
 
    /**
-    * Calendar supporting function
-    * @param view
+    * Calendar supporting function for setting date
+    * @param view The view that was clicked
     */
    @SuppressWarnings("deprecation")
    public void setDate(View view) {
@@ -401,8 +353,8 @@ public class StudentHome extends AppCompatActivity {
 
    /**
     * Calendar supporting function
-    * @param id
-    * @return
+    * @param id Object id
+    * @return Calls function to set date
     */
    @Override
    protected Dialog onCreateDialog(int id) {
